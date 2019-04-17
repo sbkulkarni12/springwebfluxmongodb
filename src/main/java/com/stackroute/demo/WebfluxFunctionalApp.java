d3b47e813e8f7191c3e5821d471569eaf2a2cd49
gyanks <riitdel@gmail.com>
2019-04-17 14:21:08 +0530

    Initial Version

---
+++ src/main/java/com/stackroute/demo/WebfluxFunctionalApp.java
@@ -0,0 +1,12 @@
+package com.stackroute.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+ 
+@SpringBootApplication
+public class WebfluxFunctionalApp {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxFunctionalApp.class, args);
+    }
+}
---
+++ src/main/java/com/stackroute/demo/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.stackroute.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stackroute.demo.dao.EmployeeRepository;
+import com.stackroute.demo.model.Employee;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+ 
+@Service
+public class EmployeeService implements IEmployeeService {
+     
+    @Autowired
+    EmployeeRepository employeeRepo;
+ 
+    public void create(Employee e) {
+        employeeRepo.save(e).subscribe();
+    }
+ 
+    public Mono<Employee> findById(Integer id) {
+        return employeeRepo.findById(id);
+    }
+ 
+    public Flux<Employee> findByName(String name) {
+        return employeeRepo.findByName(name);
+    }
+ 
+    public Flux<Employee> findAll() {
+        return employeeRepo.findAll();
+    }
+ 
+    public Mono<Employee> update(Employee e) {
+        return employeeRepo.save(e);
+    }
+ 
+    public Mono<Void> delete(Integer id) {
+        return employeeRepo.deleteById(id);
+    }
+ 
+}
